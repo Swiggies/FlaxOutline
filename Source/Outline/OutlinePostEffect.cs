57f54dd571c63c149a630493ff6276b3dcc3df50
Olly Rybak <ollyrybak@gmail.com>
2023-09-17 20:10:05 +1000

    Init

---
+++ Source/Outline/OutlinePostEffect.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using FlaxEngine;
+
+namespace Outline
+{
+    public class OutlinePostEffect : PostProcessEffect
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Data
+        {
+            public Float4 Color;
+            public Float2 TexelSize;
+            public float Size;
+            public float ColorSensitivity;
+            public float NormalSensitivity;
+            public float DepthSensitivity;
+        }
+
+        public float OutlineScale = 0.1f;
+        public float ColorSensitivity = 0;
+        public float NormalSensitivity = 2.5f;
+        public float DepthSensitivity = 100;
+
+        [Tooltip("Increases depth accuracy at low distances with less accuracy at high distances.")]
+        public float DepthAccuracy = 8;
+
+        private GPUPipelineState _psFullscreen;
+        private Shader _shader;
+        public Color Color = Color.Black;
+
+        public Shader Shader
+        {
+            get => _shader;
+            set
+            {
+                if (_shader != value)
+                {
+                    _shader = value;
+                    ReleaseShader();
+                }
+            }
+        }
+
+        public override void OnEnable()
+        {
+#if FLAX_EDITOR
+            // Register for asset reloading event and dispose resources that use shader
+            Content.AssetReloading += OnAssetReloading;
+#endif
+
+            // Register postFx to game view
+            MainRenderTask.Instance.AddCustomPostFx(this);
+        }
+
+#if FLAX_EDITOR
+        private void OnAssetReloading(Asset asset)
+        {
+            // Shader will be hot-reloaded
+            if (asset == Shader)
+                ReleaseShader();
+        }
+#endif
+
+        public override void OnDisable()
+        {
+            // Remember to unregister from events and release created resources (it's gamedev, not webdev)
+            MainRenderTask.Instance.RemoveCustomPostFx(this);
+#if FLAX_EDITOR
+            Content.AssetReloading -= OnAssetReloading;
+#endif
+            ReleaseShader();
+        }
+
+        private void ReleaseShader()
+        {
+            // Release resources using shader
+            Destroy(ref _psFullscreen);
+        }
+
+        public override bool CanRender()
+        {
+            return base.CanRender() && Shader && Shader.IsLoaded;
+        }
+
+        public override unsafe void Render(GPUContext context, ref RenderContext renderContext, GPUTexture input, GPUTexture output)
+        {
+            // Here we perform custom rendering on top of the in-build drawing
+
+            // Setup missing resources
+            if (!_psFullscreen)
+            {
+                _psFullscreen = new GPUPipelineState();
+                var desc = GPUPipelineState.Description.DefaultFullscreenTriangle;
+                desc.PS = Shader.GPU.GetPS("PS_Fullscreen");
+                _psFullscreen.Init(ref desc);
+            }
+
+            // Set constant buffer data (memory copy is used under the hood to copy raw data from CPU to GPU memory)
+            var cb = Shader.GPU.GetCB(0);
+            if (cb != IntPtr.Zero)
+            {
+                // When using more constants create structure with `StructLayout(LayoutKind.Sequential)` attribute and pass it's address to copy data
+                var data = new Data
+                {
+                    Color = Color,
+                    TexelSize = Float2.One / input.Size,
+                    Size = OutlineScale,
+                    ColorSensitivity = ColorSensitivity,
+                    DepthSensitivity = DepthSensitivity,
+                    NormalSensitivity = NormalSensitivity,
+                };
+                context.UpdateCB(cb, new IntPtr(&data));
+            }
+
+            // Draw fullscreen triangle using custom Pixel Shader
+            context.BindCB(0, cb);
+            context.BindSR(0, input);
+            context.BindSR(1, renderContext.Buffers.DepthBuffer);
+            context.BindSR(2, renderContext.Buffers.GBuffer1);
+            context.SetState(_psFullscreen);
+            context.SetRenderTarget(output.View());
+            context.DrawFullscreenTriangle();
+        }
+    }
+}
---
+++ Source/Shaders/OutlinePostEffect.shader
@@ -0,0 +1,69 @@
+﻿#include "./Flax/Common.hlsl"
+
+META_CB_BEGIN(0, Data)
+float4 Color;
+float2 TexelSize;
+float Size;
+float ColorSensitivity;
+float NormalSensitivity;
+float DepthSensitivity;
+float DepthAccuracy;
+META_CB_END
+
+Texture2D SceneColor : register(t0);
+Texture2D SceneDepth : register(t1);
+Texture2D SceneNormals : register(t2);
+
+void Outline(float2 uv, out float4 outline)
+{
+    float halfScaleFloor = floor(Size);
+    float halfScaleCeil = ceil(Size);
+    
+    float depthSamples[4];
+    float2 uvSamples[4];
+    float3 colorSamples[4], normalSamples[4];
+    
+    uvSamples[0] = uv - float2(TexelSize) * halfScaleFloor;
+    uvSamples[1] = uv + float2(TexelSize) * halfScaleCeil;
+    uvSamples[2] = uv + float2(TexelSize.x * halfScaleCeil, -TexelSize.y * halfScaleFloor);
+    uvSamples[3] = uv + float2(-TexelSize.x * halfScaleFloor, TexelSize.y * halfScaleCeil);
+    
+    for (int i = 0; i < 4; i++)
+    {
+        colorSamples[i] = SceneColor.SampleLevel(SamplerLinearClamp, uvSamples[i], 0).rgb;
+        normalSamples[i] = SceneNormals.SampleLevel(SamplerLinearClamp, uvSamples[i], 0).rgb;
+        depthSamples[i] = pow(SceneDepth.SampleLevel(SamplerLinearClamp, uvSamples[i], 0).r, DepthSensitivity);
+    }
+    
+    // Depth
+    float3 edgeFiniteDifference0 = depthSamples[1] - depthSamples[0];
+    float3 edgeFiniteDifference1 = depthSamples[3] - depthSamples[2];
+    float edgeDepth = sqrt(dot(edgeFiniteDifference0, edgeFiniteDifference0) + dot(edgeFiniteDifference1, edgeFiniteDifference1));
+    edgeDepth = edgeDepth > (1 / DepthSensitivity) ? 1 : 0;
+    
+    // Normals
+    float3 normalFiniteDifference0 = normalSamples[1] - normalSamples[0];
+    float3 normalFiniteDifference1 = normalSamples[3] - normalSamples[2];
+    float4 edgeNormal = sqrt(dot(normalFiniteDifference0, normalFiniteDifference0) + dot(normalFiniteDifference1, normalFiniteDifference1));
+    edgeNormal = edgeNormal > (1 / NormalSensitivity) ? 1 : 0;
+    
+    // Color
+    float col0 = colorSamples[1] - colorSamples[0];
+    float col1 = colorSamples[3] - colorSamples[2];
+    float4 edgeColor = sqrt(dot(col0, col1) + dot(col0, col1));
+    edgeColor = edgeColor > (1 / ColorSensitivity) ? 1 : 0;
+    
+    float edge = max(edgeDepth, max(edgeNormal, edgeColor));
+    float4 scene = SceneColor.SampleLevel(SamplerPointClamp, uv, 0);
+    float4 color = edge * Color;
+    outline = lerp(scene, color, edge);
+}
+
+META_PS(true, FEATURE_LEVEL_ES2)
+float4 PS_Fullscreen(Quad_VS2PS input) : SV_Target
+{
+	// Solid color fill from the constant buffer passed from code
+	float4 outline;
+	Outline(input.TexCoord, outline);
+	return outline;
+}
